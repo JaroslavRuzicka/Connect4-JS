fb9cbdb242c6c1117837eeb61ad1b75bf886a14d
Jaroslav Růžička <jaroslav.ruzicka22@gmail.com>
2023-11-29 20:14:07 +0100

    Created a function for calculating column coordiantes and adding them to a object, also created a function that identifies whitch column I am clickin/ hovering my mouse over. I still have to add functionality to it tho. Right now it only prints console logs

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 let pageBody = document.querySelector(".boardContainer")
 let testSquare = document.getElementsByClassName("square")
+// let firstLine = document.querySelector(".line0").children
+let columnCoordinates = {}
+
 
 let boardStateObject = {}
 
@@ -22,14 +25,20 @@ function main(){
 
     //if I use coordinates to select everytime someone resizes window I will have to recalculate coordinates of my squares 
     /*If I use the event listener on every square, I will have to add a crap ton of eventlistener otherwise I wont be able to target specific square if
-    I hit the border of the board.
+    I hit the border of the board. 
+    Adding the event listener seems to be easier choice programing vise, but it feels like I am using way to much memory for nothing
+    and the calculation that I want to do in the first step are propably done behind the scenes and I wont even have to think about then
+    Since I have no concept of knowing whitch solutin is better performace wise, I have decided to go and do the coordinates one
+    since that one seem more complicated on paper. 
+    I didnt study no sience of them puters
     */
     window.addEventListener("mousedown", event =>{
-        console.log(event.clientX)
+        //console.log(event.clientX)
+        findTheHoveredColumn(event.clientX)
     })
+
+    calculateCoordinatesOfRows()
 }
-
-
 
 function createBoard(){
     // div.textContent = "here kitty kitty"
@@ -82,9 +91,53 @@ function selectSquare(e){
     // console.log(testSquare);
 
      
-    console.log("I selected stuff", e.target)
+    //console.log("I selected stuff", e.target)
 }
 
+function calculateCoordinatesOfRows(){
+
+    let firstLine = document.querySelector(".line0").children
+    let  numOfElementsInFirstLine = firstLine.length
+    for(let i = 0; i < numOfElementsInFirstLine; i++){
+        let xLeftCoordinate = firstLine[i].getBoundingClientRect().left
+        let xRightCoordinate = firstLine[i].getBoundingClientRect().right
+        let columnName = firstLine[i].className.split(" ")[1]
+
+        columnCoordinates[columnName] = {
+            leftCoordinate: xLeftCoordinate,
+            rightCoordinate: xRightCoordinate
+
+        
+        }
+    }
+
+}
+
+//I still have to add functionality to this, right now it olny prints to the console log
+function findTheHoveredColumn(xMouseCoordinate) {
+    let numOfColumns = Object.keys(columnCoordinates).length
+    let coordinateKeys = Object.keys(columnCoordinates)
+    
+    for (let i = 0; i < numOfColumns; i++){
+        if (i == 0){
+         if(xMouseCoordinate >= columnCoordinates[coordinateKeys[i]].leftCoordinate && xMouseCoordinate <= columnCoordinates[coordinateKeys[i]].rightCoordinate){
+            console.log(`Match${i}`);
+            return 0   
+            
+            }   
+        }        
+
+
+        if(xMouseCoordinate > columnCoordinates[coordinateKeys[i]].leftCoordinate && xMouseCoordinate <= columnCoordinates[coordinateKeys[i]].rightCoordinate){
+            console.log(`Match${i}`);
+            return 0   
+            
+        }
+
+    }
+    
+    console.log("No match")
+}
 
 
 window.onload = main()
